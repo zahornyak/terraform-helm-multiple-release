59809715e361dc2852bcbb35c6159e722f2f44c7
Andriy Zahornyak <andriyzzahornyak@gmail.com>
2024-10-28 18:09:39 +0200

    fix: outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "releases" {
-  value       = helm_release.chart[*]
+  value       = { for release in helm_release.chart : release.name => release }
   description = "Helm release attributes created by this module"
 }
